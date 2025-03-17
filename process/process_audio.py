389ba4cb8c36b3b8abd3f23284cf24cbfbd6036d
Soyeon Jang <soyeonjangg@gmail.com>
2025-03-17 19:06:06 -0400

    add the rest

--- process/process_audio.py
+++ process/process_audio.py
@@ -1,51 +1,51 @@
-# import sys
-# from pywhispercpp.model import Model
 import sys
-
 from faster_whisper import WhisperModel
+from scipy.special import softmax
+import numpy as np
 
+print("HELLOP")
+# Load model directly
+import torch
+from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
 
-# Get the file path from the command-line arguments
 if len(sys.argv) < 2:
     print("Usage: python process_audio.py <file_path>")
     sys.exit(1)
 file_path = sys.argv[1]
 
-# Transcribe the audio file
-# model = Model(model="tiny")
-print("IS IT HERE")
 model_size = "large-v3"
-# model = WhisperModel(model_size, device="cpu", compute_type="int8")
-model = WhisperModel(
-    model_size, device="cpu", cpu_threads=4, compute_type="int8", language="en"
+model = WhisperModel(model_size, device="cpu", compute_type="int8")
+
+segments, info = model.transcribe(file_path, beam_size=5, language="en")
+# segments = list(segments)
+print(segments)
+# for segment in segments:
+#     print(segment.text)
+
+tokenizer = DistilBertTokenizer.from_pretrained(
+    "distilbert-base-uncased-finetuned-sst-2-english"
+)
+senti_model = DistilBertForSequenceClassification.from_pretrained(
+    "distilbert-base-uncased-finetuned-sst-2-english"
 )
 
-segments, info = model.transcribe(file_path, beam_size=5)
-segments = list(segments)
-# segments = model.transcribe(file_path)
+inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
+with torch.no_grad():
+    logits = senti_model(**inputs).logits
 
-for segment in segments:
-    print(segment.text)
-print("File processed successfully!")
+predicted_class_id = logits.argmax().item()
+senti_model.config.id2label[predicted_class_id]
 
-# Sentiment data to be sent to the server
-# sentiment = "positive"  # This could be any sentiment like 'positive', 'negative', etc.
 
-# # Now send the downloaded WAV file along with sentiment data to the server
-# with open("downloaded_recording.wav", "rb") as audio_file:
-#     files = {
-#         "audio": ("recording.wav", audio_file, "audio/wav"),  # Send the WAV file
-#     }
-#     data = {"sentiment": sentiment}  # Include the sentiment data
+# encoded_input = tokenizer(segments, return_tensors="pt")
+# output = senti_model(**encoded_input)
+# scores = output[0][0].detach().numpy()
+# scores = softmax(scores)
 
-#     # Send the POST request
-#     upload_response = requests.post(upload_url, files=files, data=data)
+# ranking = np.argsort(scores)
+# ranking = ranking[::-1]
 
-# # Check the server's response
-# if upload_response.status_code == 200:
-#     print("Successfully uploaded the file with sentiment.")
-#     print(
-#         "Server Response:", upload_response.json()
-#     )  # Assuming server returns JSON response
-# else:
-#     print(f"Error uploading the file: {upload_response.status_code}")
+# for i in range(scores.shape[0]):
+#     l = config.id2label[ranking[i]]
+#     s = scores[ranking[i]]
+#     print(f"{i+1}) {l} {np.round(float(s), 4)}")
